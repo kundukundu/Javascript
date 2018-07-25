65a4dc4324c62a8965b314788c788f852d67457f
Chiyana Simões <chiyana@gmail.com>
2018-07-25 15:18:09 +0200

    Verfica si la variable está vacia

---
+++ estaVacia.js
@@ -0,0 +1,43 @@
+/**
+* Verifica si la variable está vaci
+*/
+
+function estaVacia(variable) {
+        let type = typeof variable;
+        if(type === 'undefined') {
+            return true;
+        }
+        if(type=== 'boolean') {
+            return !variable;
+        }
+        if(variable === null) {
+            return true;
+        }
+        if(variable === undefined) {
+            return true;
+        }
+        if(variable instanceof Array) {
+            if(variable.length < 1) {
+                return true;
+            }
+        }
+        else if(type === 'string') {
+            if(variable.length < 1) {
+                return true;
+            }
+            if(variable==='0') {
+                return true;
+            }
+        }
+        else if(type === 'object') {
+            if(Object.keys(variable).length < 1) {
+                return true;
+            }
+        }
+        else if(type === 'number') {
+            if(variable===0) {
+                return true;
+            }
+        }
+        return false;
+    }
